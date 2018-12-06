5d6d94b63d1be5c4eca7568b251202636fb92860
James Bulpin <James.Bulpin@citrix.com>
2018-12-06 13:56:49 +0000

    Multiple changes

--- ESP8266Badge.ino
+++ ESP8266Badge.ino
@@ -2,7 +2,9 @@
 
 #include <Arduino.h>
 
+#include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include "SSD1306.h"
 #include <ArduinoJson.h>
@@ -10,14 +12,23 @@
 
 #define USE_SERIAL Serial
 
+#define WS2811_PIN 0
+#define WS2811_LED_COUNT 1
+
+ESP8266WiFiMulti WiFiMulti;
+
 SSD1306 display(0x3c, SDA, SCL, OLED_RST, GEOMETRY_128_32);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(WS2811_LED_COUNT, WS2811_PIN /*, NEO_GRB + NEO_KHZ800*/);
+
+#include "config.h" // Local wifi, API, and other configuration
 
 void startWiFi() {
   WiFi.mode(WIFI_STA);
-  WiFi.begin("<ssid>", "<psk>");
+  setupLocalWifiConfig(WiFiMulti);
+  //WiFiMulti.addAP("<SSID>", "<PSK>");
 
   // Wait for WiFi connection
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFiMulti.run() != WL_CONNECTED) {
     delay(250);
     USE_SERIAL.print('.');
   }
@@ -32,6 +43,14 @@ void startWiFi() {
 void setup() {
   USE_SERIAL.begin(115200);
 
+  strip.begin();
+
+  // Initialize LEDs to off
+  for (int i = 0; i < WS2811_LED_COUNT; i++) {
+    strip.setPixelColor(i, strip.Color(255, 0, 0));
+  }
+  strip.show();
+  
   // Initialise the OLED display and show the Citrix logo
   display.init();
   display.flipScreenVertically();
@@ -76,42 +95,55 @@ void loop() {
     prevDisplay = currentMillis;
 
     // Only update the display if we have a valid event description and target time
-    if ((timerTarget > 0) && (strlen(description) > 0)) {
+    if (strlen(description) > 0) {
       display.clear();
       if (currentMillis < showLogoUntil) {
         // Show the Citrix logo
         display.drawXbm(0, 0, citrix_logo_width, citrix_logo_height, citrix_logo);        
       }
       else {
-        // Show a countdown timer and the event description
-        unsigned long remaining = (timerTarget < currentSecs)?0:(timerTarget - currentSecs);
-        unsigned long days = remaining/86400;
-        unsigned long hours = (remaining%86400)/3600;
-        unsigned long minutes = (remaining%3600)/60;
-        unsigned long seconds = remaining%60;
-        sprintf(remainingText, "%u %s %u:%02u", days, (days==1)?"day":"days", hours, minutes);
-        display.setFont(ArialMT_Plain_16);
-        display.drawString(0, 0, remainingText);
-        int16_t x = display.getStringWidth(remainingText);
-        display.setFont(ArialMT_Plain_10);
-        sprintf(remainingText, ":%02u", seconds);
-        display.drawString(x, 5, remainingText);
-        display.setFont(ArialMT_Plain_16);
-        display.drawString(0, 16, "Until");
-        display.drawString(display.getStringWidth("Until "), 16, description);
+        if (timerTarget > 0) {
+          // Show a countdown timer and the event description
+          unsigned long remaining = (timerTarget < currentSecs)?0:(timerTarget - currentSecs);
+          unsigned long days = remaining/86400;
+          unsigned long hours = (remaining%86400)/3600;
+          unsigned long minutes = (remaining%3600)/60;
+          unsigned long seconds = remaining%60;
+          sprintf(remainingText, "%u %s %u:%02u", days, (days==1)?"day":"days", hours, minutes);
+          display.setFont(ArialMT_Plain_16);
+          display.drawString(0, 0, remainingText);
+          int16_t x = display.getStringWidth(remainingText);
+          display.setFont(ArialMT_Plain_10);
+          sprintf(remainingText, ":%02u", seconds);
+          display.drawString(x, 5, remainingText);          
+          display.drawString(0, 21, "Until");
+          int indent = display.getStringWidth("Until ");
+          display.setFont(ArialMT_Plain_16);
+          display.drawString(indent, 16, description);
+        }
+        else {
+          // Just show the text message
+          display.setFont(ArialMT_Plain_16);
+          display.drawString(0, 8, description);
+        }
       }
     }
     display.display();
   }
 
   // Refetch the event description and remaining time every minute
-  if ((currentMillis - prevHTTP > intervalHTTP) || (prevHTTP == 0)){
+  if (((currentMillis - prevHTTP > intervalHTTP) || (prevHTTP == 0)) && (WiFiMulti.run() == WL_CONNECTED)) {
+    USE_SERIAL.print("SSID: ");
+    USE_SERIAL.println(WiFi.SSID());
+    USE_SERIAL.print("IP: ");
+    USE_SERIAL.println(WiFi.localIP());
+  
     prevHTTP = currentMillis;
 
     HTTPClient http;
 
     // The second argument here is the SSL fingerprint
-    http.begin("<API endpoint>", "7b c9 47 ae f4 1e f6 79 f9 b5 40 29 07 61 7b 66 93 17 5b 68");
+    http.begin(API_ENDPOINT, API_ENDPOINT_CERT_FINGERPRINT);
     int httpCode = http.GET();
 
     if (httpCode > 0) {
@@ -126,7 +158,7 @@ void loop() {
         if (!root.success()) {
           USE_SERIAL.println("parseObject() failed");
         }
-        else {
+        else if (root.containsKey("description")) {
           if (strcmp(description, root["description"]) != 0) {
             // New text, show the logo for a few seconds before changing
             showLogoUntil = currentMillis + 3000;
@@ -138,6 +170,10 @@ void loop() {
           // actual time.
           timerTarget = root["remaining"].as<unsigned long>() + currentMillis/1000;
           display.display();
+        }
+        else if (root.containsKey("text")) {
+          strncpy(description, root["text"], 19);
+          timerTarget = 0;
         }
       }
     } else {
